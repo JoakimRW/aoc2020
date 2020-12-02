961b06413fcb5b791eab9a722e9f9661728155a4
Wallerius Joakim <joakim.wallerius1@saabgroup.com>
2020-12-02 10:14:25 +0100

    day2A

---
+++ AOC2020/src/twoA/TwoA.java
@@ -0,0 +1,30 @@
+package twoA;
+
+public class TwoA {
+
+	public static void main(String[] args) {
+
+		int correctCounter = 0;
+		for (int i = 0; i < args.length; i = i + 3) {
+
+			String times = args[i];
+			String character = args[i + 1];
+			String password = args[i + 2];
+
+			String[] split = times.split("-");
+			Integer timesMin = Integer.valueOf(split[0]);
+			Integer timesMax = Integer.valueOf(split[1]);
+
+			String test = password.replaceAll("[^" + character + "]", "");
+			int length = test.length();
+
+			if (length >= timesMin && length <= timesMax) {
+				correctCounter++;
+			}
+		}
+
+		System.out.println(correctCounter);
+
+	}
+
+}
